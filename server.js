87c665df3e3e5d661258003eeec38bd1e23ed294
picsou-31 <gdhdszsmg7@privaterelay.appleid.com>
2026-08-15 23:12:39 +0100

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const session = require('express-session');
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const SteamStrategy = require('passport-steam').Strategy;
 
 const app = express();
 
-// إعدادات الاستراتيجيات هنا
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
